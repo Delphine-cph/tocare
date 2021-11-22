066c033ac1d562e90338c78ddf8cab2aaf78c618
Lucien Dupre <duprelucien@gmail.com>
2021-11-22 17:15:11 +0100

    add controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,43 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_22_141024) do
+ActiveRecord::Schema.define(version: 2021_11_22_155941) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cares", force: :cascade do |t|
+    t.bigint "patient_id", null: false
+    t.bigint "user_id", null: false
+    t.date "day"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["patient_id"], name: "index_cares_on_patient_id"
+    t.index ["user_id"], name: "index_cares_on_user_id"
+  end
+
+  create_table "patients", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.text "soin"
+    t.integer "age"
+    t.text "adress"
+    t.integer "telephone_number"
+    t.integer "telephone_family"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "replacements", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.bigint "user_id", null: false
+    t.bigint "patient_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["patient_id"], name: "index_replacements_on_patient_id"
+    t.index ["user_id"], name: "index_replacements_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,8 +56,16 @@ ActiveRecord::Schema.define(version: 2021_11_22_141024) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "adeli_number"
+    t.string "arrondissement"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "cares", "patients"
+  add_foreign_key "cares", "users"
+  add_foreign_key "replacements", "patients"
+  add_foreign_key "replacements", "users"
 end

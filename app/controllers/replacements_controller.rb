f4064d182ddcd7ed5e0d07a0adc7b8a7d3b83737
Lucien Dupre <duprelucien@gmail.com>
2021-11-24 11:46:05 +0100

    index reflection

--- app/controllers/replacements_controller.rb
+++ app/controllers/replacements_controller.rb
@@ -2,9 +2,10 @@ class ReplacementsController < ApplicationController
 
   def index
     # @user.where(@replacement.start_date >= user.start_date && @replacement.end_date <= @user.end_date)
-    @user = User.first
-    Remplacement.where("start_date >= ?", @user.start_date)
-                .where("end_date <= ?", @user.end_date)
+    @user = User.first # params[:query][:start_date]  && params[:query][:end_date]
+    @replacements = User.where("start_date >= ?", @user.start_date) # start date need to come from form
+        .where("end_date <= ?", @user.end_date) # end date need to come from form
+        # Filter for replacements WITHOUT owner_id (not yet "accepted")
   end
 
 

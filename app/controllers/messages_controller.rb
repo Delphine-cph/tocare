c67e62aa8c8ec7ac8ec48915045877d7d7ec50bc
Delphine ChangPiHin <delphine.cph@gmail.com>
2021-11-24 16:42:06 +0100

    envoi de messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,8 +6,18 @@ class MessagesController < ApplicationController
     @message.user = current_user
     if @message.save
       redirect_to chatroom_path(@chatroom, anchor: "message-#{@message.id}")
+      ChatroomChannel.broadcast_to(
+        @chatroom,
+        render_to_string(partial: "message", locals: { message: @message })
+      )
     else
       render "chatrooms/show"
     end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content)
   end
 end

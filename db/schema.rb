743aa63c41f79d0a8ff77240211213109f9a2538
Delphine ChangPiHin <delphine.cph@gmail.com>
2021-11-25 12:30:40 +0100

    style du formulaire partie 1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_24_145911) do
+ActiveRecord::Schema.define(version: 2021_11_25_103809) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 2021_11_24_145911) do
     t.integer "telephone_family"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "genre"
   end
 
   create_table "replacements", force: :cascade do |t|
